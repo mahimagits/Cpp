bec7e187380402c94f4ff9fb0b013d26d922f20e
Mahima <mahimajha2607@gmail.com>
2026-01-27 19:19:26 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    cout << "Mahima Jha";
+int main() {
+    int n;
+    cout << "Enter a number";
+    cin >> n;
+    int count = 1;
+    int sum = 0;
+    while (count <= n) {
+        sum += count;
+        count += 1;
+    }
+    cout << sum;
     return 0;
 }
