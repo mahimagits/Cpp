d6ab57437bb5fb3f5fb144334241a5b3979e923f
Mahima <mahimajha2607@gmail.com>
2026-01-28 16:17:43 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 int main() {
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
-    if (age >= 18) {
-        cout << "Can Vote";
-    }else {
-        cout << "Cannot Vote";
+    int marks;
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "Grade - A";
+    } else if (marks < 90 && marks >=80) {
+        cout << "Grade - B";
+    } else {
+        cout << "Grade - C";
     }
     return 0;
 }
