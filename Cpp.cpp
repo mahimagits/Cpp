c974ac568540eb2a7df51cba43525f4ff8629b76
Mahima <mahimajha2607@gmail.com>
2026-02-02 21:16:12 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -5,8 +5,12 @@ int main() {
     int n, sum = 0;
     cout << "Enter a number: ";
     cin >> n;
-    for (int i = 1; i <= n; i+2){
-        sum +=i;
+    for (int i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            continue;
+        } else {
+            sum += i;
+        }
     }
     cout << sum << endl;
     return 0;
