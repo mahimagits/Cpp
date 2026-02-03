065744dff933c7d69fb88d774202afed420589d2
Mahima <mahimajha2607@gmail.com>
2026-02-03 21:10:50 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// int main() {
-//     int n;
-//     cout << "Enter a number: ";
-//     cin >> n;
-//     for(int i = 2; i < n; i++){
-//         if (n%i == 0){
-//             cout << "The given number is not a prime number";
-//             break;
-//         } else {
-//             cout << "The given number is a PRIME NUMBER";
-//             break;
-//         }
-//     }
-//     return 0;
-// }
-
-int main(){
-    cout << "Mahima Jha";
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i = 2; i < n; i++){
+        if (n%i == 0){
+            cout << "The given number is not a prime number";
+            break;
+        } else {
+            cout << "The given number is a PRIME NUMBER";
+            break;
+        }
+    }
     return 0;
 }
+
