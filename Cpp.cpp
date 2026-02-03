334f499ef2bb330fe6781171692713f1452bf09b
Mahima <mahimajha2607@gmail.com>
2026-02-03 21:14:13 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for(int i = 2; i < n; i++){
-        if (n%i == 0){
-            cout << "The given number is not a prime number";
-            break;
-        } else {
-            cout << "The given number is a PRIME NUMBER";
+int main(){
+    for(int i = 1; i <= 100; i++){
+        if (i%2 == 0) {
+            cout << i << endl;
+        }
+        if (i == 20) {
             break;
         }
     }
-    return 0;
 }
 
