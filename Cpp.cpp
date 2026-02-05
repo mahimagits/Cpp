b020429de8e5e3e4291608f5ec4b9f6b5a926066
Mahima <mahimajha2607@gmail.com>
2026-02-05 23:09:02 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    for(int i = 2; i <= n-1; i++){
-        if(n%i == 0){
-            cout << "Non Prime";
-            break;
-        }else{
-            cout << "Prime";
-            break;
-        }
-    }
+    cout << "I love coding";
     return 0;
 }
