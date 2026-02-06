b2685f465c5701892fd7127d36e99af810fa4789
Mahima <mahimajha2607@gmail.com>
2026-02-06 13:25:47 +0530

    file change

--- Cpp.cpp
+++ Cpp.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "I love coding";
-    return 0;
+class Parent {
+    public:
+        int a = 10;
+    protected:
+        int b = 20;
+    private:
+        int c = 30;
+};
+
+class Child : public Parent {
+    public:
+        void show () {
+            cout << a << endl;
+            cout << b << endl;
+            // cout << c << endl; //private
+        }
+};
+
+// class Child : protected Parent {
+//     public:
+//         void show(){
+//             cout << a << endl;
+//             cout << b << endl;
+//         }
+// };
+
+// class Child : private Parent {
+//     public:
+//         void show(){
+//             cout << a << endl;
+//             cout << b << endl;
+//         }
+// };
+
+int main() {
+    Child obj;
+    cout << obj.a << endl;
 }
