3c7b59bb71062bc9be02a81a08f85aa538b6c7a6
Mahima <mahimajha2607@gmail.com>
2026-02-14 23:45:47 +0530

    file chane

--- first.cpp
+++ first.cpp
@@ -1,72 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class Complex{
-    public:
-    int real, imag;
-
-    Complex(int r = 0, int i = 0){
-        real = r;
-        imag = i;
-    }
-    Complex operator + (Complex obj) {
-        Complex temp;
-        temp.real = real + obj.real;
-        temp.imag = imag + obj.imag;
-        return temp; 
-    }
-};
-
-// int main() {
-//     Complex c1(2, 3), c2(4, 5);
-//     Complex c3 = c1 + c2;
-
-//     cout << c3.real << " + " << c3.imag << "i";
-//     return 0;
-// }
-
-class Count{
-    public:
-        int value;
-
-        Count(int v = 0){
-            value = v;
-        }
-
-        void operator ++ () {
-            value++;
-        }
-}; 
-
-// int main() {
-//     Count c(5);
-//     ++c;
-
-//     cout << c.value;
-//     return 0;
-// }
-
-class Person {
-    public:
-        int age;
-
-        Person(int a){
-            age = a;
-        }
-
-        bool operator == (Person p) {
-            return age == p.age;
-        }
-};
-
 int main(){
-    Person p1(20), p2(21);
-
-    if (p1 == p2){
-        cout << "Same Age";
-    } else {
-        cout << "Different Age";
+    int n = 5;
+    for(int i = 0; i < n; i++){
+        cout << "*"<< endl;
     }
-    return 0;
 }
-
