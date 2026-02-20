25591c752aa415d426d0df6951f9c79396faaa39
Mahima <mahimajha2607@gmail.com>
2026-02-20 20:43:45 +0530

    file change

--- first.cpp
+++ first.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Enter a number:";
+    cout << "Enter a number: ";
     cin >> n;
+    char ch = 'A';
     for (int i = 0; i < n; i++){
-        int m = n;
-        for (int j = 0; j < m; j++){
-            cout << "*" << " ";
+        for (int j = 0; j < n; j++){
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
