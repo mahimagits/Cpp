6a83fc2e58433f70c86d2d3bd5d27b609ae4ae2a
Mahima <mahimajha2607@gmail.com>
2026-02-21 18:55:22 +0530

    file change

--- first.cpp
+++ first.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main(){
     int n = 4;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = n; j > i+1; j--){
             cout << " ";
         }
-        for(int k = n; k > i; k--){
-            cout << i+1;
+        for (int k = 0; k < i+1; k++){
+            cout << k+1;
+        }
+        for (int l = i; l > 0; l--){
+            cout << l;
         }
         cout << endl;
     }
