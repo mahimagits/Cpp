73debceca354539e178748f1fabb9f0e4b0efb53
Mahima <mahimajha2607@gmail.com>
2026-02-21 18:31:13 +0530

    file change

--- first.cpp
+++ first.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    char ch = 'A';
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << ch << " ";
-            ch++;
+int main(){
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int k = n; k > i; k--){
+            cout << i+1;
         }
         cout << endl;
     }
