d6249e73974e5d9f9474f446ea81e4de158ea126
Mahima <mahimajha2607@gmail.com>
2026-02-22 19:14:15 +0530

    file change

--- first.cpp
+++ first.cpp
@@ -1,106 +1,14 @@
 #include <iostream>
 using namespace std;
 
-//function definition
-int printHello(){
-    cout << "Hello World\n";
-    return 3;
-}
-
-//Calculate sum of two numbers
-// int calcSum(int a, int b){
-//     return a+b;
-// }
-
-// calculate minimum of two numbers
-int min(int a, int b){
-    if(a > b){
-        return b;
-    }else {
-        return a;
-    }
-}
-
-// Calculate sum of numbers from 1 to n
-int calcSum(int n){
-    int sum = 0;
-    for(int i = 0; i <=n; i++){
-        sum += i;
-    }
-    return sum;
-}
-
-int calcFact(int n){
-    int fact = 1;
-    for(int i = 1; i <= n; i++){
-        fact *= i;
-    }
-    return fact;
-}
-
-int calcDigitSum(int num){
-    int digitSum = 0;
-    while(num > 0){
-        int n = num % 10;
-        digitSum += n;
-        num = num/10;
-    }
-    return digitSum;
-}
-
-int calcBinCoeff(int n, int r){
-    if(n > r){
-    int nFact = 1;
-    for(int i = 1; i <= n; i++){
-        nFact *= i;
-    }
-    int rFact = 1;
-    for(int i = 1; i <= r ; i++){
-        rFact *= i;
-    }
-    int fact = 1;
-    for(int i = 1; i <= (n-r); i++){
-        fact *= i;
-    }
-    return nFact/(rFact*fact);
-    } else {
-        return false;
-    }
-}
-
-void isPrime(int n){
-    for(int i = 2; i < n; i++){
-        if (n%i == 0){
-            cout << "Non Prime Number";
-            break;
-        }else{
-            cout << "Prime Number";
-            break;
-        }
-    }
-}
-
-void primeNum(int n){
-    for(int i = 2; i < n; i++){
-        if(n%i == 0){
-            i++;
-        }else{
-            cout << i << endl;
-        }
-    }
-}
-
-int decToBin(int n){
-    int ans = 0, pow = 1;
-    while(n > 0){
-        int rem = n%2;
-        n = n/2;
-        ans += rem*pow;
-        pow *= 10;
-    }
-    return ans;
-}
 int main(){
-    cout << decToBin(9) << endl;
-    return 0;
+    int binNum = 110010;
+    int ans = 0, pow = 1;
+    while(binNum > 0){
+        int rem = binNum % 10;
+        binNum /= 10;
+        ans += (rem*pow);
+        pow *= 2;
+    }
+    cout << ans;
 }
