85ee4af29c0b3076b882cb8cf42aeea77d398068
Mahima <mahimajha2607@gmail.com>
2026-02-24 22:22:59 +0530

    file change

--- first.cpp
+++ first.cpp
@@ -14,15 +14,22 @@ int main(){
     // }
     // cout << ans;
 
-    int binNum;
-    cout << "Enter a binary number:";
-    cin >> binNum;
-    int ans = 0, pow = 1;
-    while (binNum > 0){
-        int rem = binNum%10;
-        binNum = binNum / 10;
-        ans += rem*pow;
-        pow *= 2;
-    }
-    cout << ans;
+    // int binNum;
+    // cout << "Enter a binary number:";
+    // cin >> binNum;
+    // int ans = 0, pow = 1;
+    // while (binNum > 0){
+    //     int rem = binNum%10;
+    //     binNum = binNum / 10;
+    //     ans += rem*pow;
+    //     pow *= 2;
+    // }
+    // cout << ans;
+
+    // cout << (6 & 10) << endl;
+    // cout << (6 | 10) << endl;
+    // cout << (6 ^ 10)<< endl;
+    // cout << (10 << 2) << endl;
+    // cout << (10 >> 1) << endl;
+    cout << !(5 > 6);
 }
