26c31b10220f075455fd4124ff921cea6a8b0cc2
Mahima <mahimajha2607@gmail.com>
2026-06-02 21:05:58 +0530

    practice

--- forLoop.cpp
+++ forLoop.cpp
@@ -19,5 +19,32 @@ int main() {
         sum += i;
     }
     cout << "Sum: " << sum << endl;
+
+    //Fibonacci series
+    int n = 10, a = 0, b = 1;
+    cout << a << " " << b << " ";
+    for(int i = 1; i <= n; i++){
+        int nextNum = a + b;
+        cout << nextNum << " ";
+        a = b;
+        b = nextNum;
+    }
+
+    //Prime Number
+    int n;
+    bool isPrime = true;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i = 2; i < n; i++){
+        if(n%i == 0){
+            isPrime = false;
+        }
+    }
+    if(isPrime){
+        cout << "Prime Number";
+    }else{
+        cout << "Non Prime Number";
+    }
+
     return 0;
 }
