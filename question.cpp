e666242e83f7439357b7dd7839065f996e8aff06
Mahima <mahimajha2607@gmail.com>
2026-06-04 22:33:26 +0530

    practice

--- question.cpp
+++ question.cpp
@@ -2,56 +2,56 @@
 using namespace std;
 
 int main(){
-    // int n;  //For example - 26
-    // cout << "Enter a number: ";
-    // cin >> n;
-    // int sum = 0;
-    // int product = 1;
-    // while(n % 10 != 0){
-    //     int rem = n % 10;
-    //     sum += rem;
-    //     product *= rem;
-    //     n = n/10;
-    // }
-    // cout << "Sum: " << sum << endl; //2+6 = 8
-    // cout << "Product: " << product << endl; //2*6 = 12
+    int n;  //For example - 26
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = 0;
+    int product = 1;
+    while(n % 10 != 0){
+        int rem = n % 10;
+        sum += rem;
+        product *= rem;
+        n = n/10;
+    }
+    cout << "Sum: " << sum << endl; //2+6 = 8
+    cout << "Product: " << product << endl; //2*6 = 12
 
     //Reverse integer
-    // int n;
-    // cout << "Enter a number: ";
-    // cin >> n;
-    // int ans = 0;
-    // while(n != 0){
-    //     int digit = n % 10;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int ans = 0;
+    while(n != 0){
+        int digit = n % 10;
        
-    //     ans = (ans * 10) + digit;
-    //     n = n/10;
-    // }
-    // if((ans > INT32_MAX / 10) || (ans < INT32_MIN / 10)){
-    //         cout << "Reverse Integer : " << 0 << endl;
-    //     } else {
-    //         cout << "Reverse Integer : " << ans << endl;
-    //     }
+        ans = (ans * 10) + digit;
+        n = n/10;
+    }
+    if((ans > INT32_MAX / 10) || (ans < INT32_MIN / 10)){
+            cout << "Reverse Integer : " << 0 << endl;
+        } else {
+            cout << "Reverse Integer : " << ans << endl;
+        }
     
     //Compliment Base 10 Integer
-    // int n;
-    // cout << "Enter a number: ";
-    // cin >> n;
-    // int m = n;
-    // int mask = 0;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int m = n;
+    int mask = 0;
 
-    // //edge - case
-    // if (n == 0){
-    //     cout << 1 << endl;
-    // }else{
-    // while (m != 0){
-    //     mask = (mask << 1) | 1;
-    //     m = (m >> 1);
-    // }
+    //edge - case
+    if (n == 0){
+        cout << 1 << endl;
+    }else{
+    while (m != 0){
+        mask = (mask << 1) | 1;
+        m = (m >> 1);
+    }
 
-    // int ans = (~n) & mask;
-    // cout << ans;
-    // }
+    int ans = (~n) & mask;
+    cout << ans;
+    }
 
     int n;
     cout << "Enter a number : ";
@@ -70,4 +70,5 @@ int main(){
     } else {
         cout << "False" << endl;
     }
+    return 0;
 }
