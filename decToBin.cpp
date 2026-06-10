33c01695822657a1904df087376adc2ad0db8718
Mahima <mahimajha2607@gmail.com>
2026-06-10 19:14:21 +0530

    practice

--- decToBin.cpp
+++ decToBin.cpp
@@ -15,17 +15,5 @@ int main(){
     }
     cout << "Binary number : " << ans << endl;
 
-//Binary to Decimal
-    int n;
-    cout << "Enter a binary number : ";
-    cin >> n;
-    int ans = 0, power = 1;
-    while(n != 0){
-        int rem = n % 10;
-        ans += (rem * power);
-        n = n/10;
-        power *= 2;
-    }
-    cout << "Decimal Number : " << ans << endl;
     return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -2,17 +2,54 @@
 using namespace std;
 
 int main(){
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // int sum = 0, prod = 1;
+    // while(n > 0){
+    //     int digit = n % 10;
+    //     sum += digit;
+    //     prod *= digit;
+    //     n /= 10;
+    // }
+    // int diff = prod - sum;
+    // cout << "Difference = " << diff << endl;
+
+    // int n;
+    // cout << "Enter a decimal number: ";
+    // cin >> n;
+    // int ans = 0, pow = 1;
+    // while(n != 0){
+    //     int digit = n % 2;
+    //     ans += (digit * pow);
+    //     pow *= 10;
+    //     n /= 2;
+    // }
+    // int count = 0;
+    // while(ans > 0){
+    //     int rem = ans % 10;
+    //     if( rem == 1){
+    //         count ++ ;
+    //     }
+    //     ans = ans/10;
+    // }
+    // cout << "Count = " << count << endl;
+
+    //Complement of Base - 10 integer
     int n;
-    cout << "Enter a number: ";
+    cout << "enter a number: ";
     cin >> n;
-    int sum = 0, prod = 1;
-    while(n > 0){
-        int digit = n % 10;
-        sum += digit;
-        prod *= digit;
-        n /= 10;
+    int m = n;
+    int mask = 0;
+    if (n == 0){
+        cout << 1 << endl;
+    } else{
+    while(m > 0){
+    mask = (mask << 1) | 1;
+    m = m >> 1;
     }
-    int diff = prod - sum;
-    cout << "Difference = " << diff << endl;
+    int ans = (~n) & mask;
+    cout << ans;
+    }
     return 0;
 }
