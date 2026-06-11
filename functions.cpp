fdc632dc6cf9c3211ae411c87db75f5567908d3e
Mahima <mahimajha2607@gmail.com>
2026-06-11 14:54:25 +0530

    functions

--- functions.cpp
+++ functions.cpp
@@ -9,9 +9,36 @@ int power(int a, int b){
     return ans;
 }
 
+string evenOrOdd(int num) {
+    if (num % 2 == 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+int nCr(int n, int r){
+    int num = 1;
+    for(int i = n; i > 0; i--){
+        num *= i;
+    }
+    int den1 = 1;
+    for(int i = r; i > 0; i--){
+        den1 *= i;
+    }
+    int den2 = 1;
+    for(int i = (n - r); i > 0; i--){
+        den2 *= i;
+    }
+    int ans = (num)/(den1 * den2);
+    return ans;
+}
+
 int main(){
     cout << power(6, 3) << endl;
-    cout << power(9, 2) << endl;
+    cout << evenOrOdd(6) << endl;
+    cout << evenOrOdd(7) << endl;
+    cout << nCr(8, 2) << endl;
 
     return 0;
 }
