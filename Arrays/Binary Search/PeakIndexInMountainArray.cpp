41de9c36e56d22df9b9c32670e29b8be4fe6b1d7
Mahima <mahimajha2607@gmail.com>
2026-06-16 20:34:31 +0530

    practice

---
+++ Arrays/Binary Search/PeakIndexInMountainArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int peakIndexInMountainArray(int arr[], int size){
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start)/2);
+    while(start < end){
+        if(arr[mid] > arr[mid - 1] & arr[mid] > arr[mid + 1]){
+            return mid;
+        } else if(arr[mid] < arr[mid - 1]){
+            end = mid;
+        } else if(arr[mid] < arr[mid + 1]){
+            start = mid;
+        }
+        mid = start + ((end - start)/2);
+    }
+}
+
+int main() {
+    int num[7] = {3, 5, 7, 9, 11, 13, 1};
+
+    cout << "Peak Index in mountain array: " << peakIndexInMountainArray(num, 7) << endl;
+
+    return 0;
+}
--- Arrays/Binary Search/firstAndLastPosition.cpp
+++ Arrays/Binary Search/firstAndLastPosition.cpp
@@ -1,3 +1,54 @@
 #include <iostream>
 using namespace std;
 
+int firstOcc(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start)/2);
+    int ans = -1;
+    while(start <= end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid - 1;
+        } else if(arr[mid] < key){
+            start = mid + 1;
+        } else{
+            end = mid - 1;
+        }
+        mid = start + ((end - start)/2);
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start)/2);
+    int ans = -1;
+    while(start <= end){
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        } else if(arr[mid] < key){
+            start = mid + 1;
+        } else{
+            end = mid - 1;
+        }
+        mid = start + ((end - start)/2);
+    }
+    return ans;
+}
+
+int main(){
+    int num[7] = {3, 5, 6, 9, 10, 10, 10};
+
+    pair<int, int> p;
+    p.first = firstOcc(num, 7, 10);
+    p.second = lastOcc(num, 7, 10);
+
+    cout << p.first << " " << p.second << endl;
+
+    cout << "Total no. of occurances of 10 is " << (p.second - p.first) + 1;
+
+    return 0;
+}
