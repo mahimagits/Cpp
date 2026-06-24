9b6a926434a4f0f045714a01c7fbf48a25014e33
Mahima <mahimajha2607@gmail.com>
2026-06-24 21:17:43 +0530

    Selection Sort

---
+++ Arrays/Sorting/selectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector <int> &arr, int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+void selectionSort(vector <int> &arr, int n){
+    for(int i = 0; i < n - 1; i++){
+        int minInd = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[minInd]){
+                minInd = j;
+            }
+        }
+        swap(arr[i], arr[minInd]);
+    }
+}
+
+int main(){
+    vector<int> num = {24, 10, 15, 2, 0, 74};
+
+    cout << "Before Sorting: " << endl;
+    printVector(num, num.size());
+
+    selectionSort(num, num.size());
+    cout << "\nAfter Sorting: " << endl;
+    printVector(num, num.size());
+
+    return 0;
+}
