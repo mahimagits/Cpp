5dc90b29c876973d59ee1b2bdf7c4656c895cbb5
Mahima <mahimajha2607@gmail.com>
2026-06-26 15:09:11 +0530

    Bubble Sort

--- Arrays/Sorting/bubbleSort.cpp
+++ Arrays/Sorting/bubbleSort.cpp
@@ -9,8 +9,8 @@ void printVector(vector<int> &arr, int n){
 }
 
 void bubbleSort(vector<int> &arr, int n){
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < n-1; j++){
+    for(int i = 1; i < n; i++){   
+        for(int j = 0; j < n-i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
