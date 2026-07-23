f3deaf0a0497453b25da7e15e94f2f9bd48f07d9
Mahima <mahimajha2607@gmail.com>
2026-07-23 23:06:15 +0530

    Practice

---
+++ 2DArrays/search2DMatrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target){
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int start = 0;
+    int end = row*col - 1;
+    int mid = start + (end - start)/2;
+    while(start <= end){
+        int element = matrix[mid/col][mid%col];
+        if(target == element){
+            return 1;
+        } else if(element < target){
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+        mid =  start + (end - start)/2;
+    }
+    return 0;
+}
+
+int main(){
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    int target;
+    cout << "Enter a number : ";
+    cin >> target;
+
+    cout << target << " is present or not : " << searchMatrix(matrix, target);
+
+    return 0;
+}
