c2c1166d49d0bc614ecdda22f99f4ece05ccf798
Mahima <mahimajha2607@gmail.com>
2026-09-08 23:08:51 +0530

    Practice

---
+++ questions/patterns.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+    // for(int i = 1; i <= n; i++){
+    //         //spaces
+    //     for(int j = 1; j <= (n-i); j++){
+    //         cout << " ";
+    //     }
+    //     for(int j = 1; j <= i; j++){
+    //         cout << "*";
+    //     }
+    //     for(int j = 1; j < i; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+        //     for(int i = 0; i < n; i++){
+        //     for(int j = n; j > i; j--){
+        //         cout << "*";
+        //     }
+        //     cout << endl;
+        // }
+
+        //         for(int i = 0; i < n; i++){
+        //     for(int j = 1; j <= (n-i); j++){
+        //         cout << j;
+        //     }
+        //     cout << endl;
+        // }
+
+        for(int i = 0; i < n; i++){
+            
+            for(int j = 0; j < i; j++){
+                cout << " ";
+            }
+            for(int j = n; j > i; j--){
+                cout << "*";
+            }
+
+            for(int j = (n-1); j > i; j--){
+                cout << "*";
+            }
+
+            cout << endl;
+        }
+    return 0;
+}
