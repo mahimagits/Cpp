07d40199aab49a591cfbb736a03c0c639a5a8760
Mahima <mahimajha2607@gmail.com>
2026-09-15 21:21:24 +0530

    Practice

---
+++ Arrays/Sorting/quickSort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int> &arr, int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int partitionInd(vector<int>& arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i < j){
+        while(arr[i] <= pivot && i <= high - 1){
+            i++;
+        }
+        while(arr[j] > pivot && j >= low + 1){
+            j--;
+        }
+        if(i < j){
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quickSort(vector<int>& arr, int low, int high){
+    if(low < high){
+        int pInd = partitionInd(arr, low, high);
+        quickSort(arr, low, pInd-1);
+        quickSort(arr, pInd + 1, high);
+    }
+}
+
+int main(){
+    vector<int> nums = {4, 19, 3, 17, 30, 78};
+
+    cout << "Before sorting: " << endl;
+    printVector(nums, nums.size());
+
+    cout << "\nAfter Sorting: " << endl;
+    quickSort(nums, 0, nums.size() - 1);
+    printVector(nums, nums.size());
+
+    return 0;
+}
